7fbe4618504164f139388d703261fd5b1c0c9031
pingu0223 <1875894522@qq.com>
2026-07-09 14:29:54 +0800

    Create code.cpp

---
+++ DP问题/练习/746 使用最小花费爬楼梯/code.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int dp0 = 0;
+        int dp1 = 0;
+        for (int i = 2; i <= cost.size(); i++) {
+            int dpi = min(dp1 + cost[i - 1], dp0 + cost[i - 2]);
+            dp0 = dp1; // 记录一下前两位
+            dp1 = dpi;
+        }
+        return dp1;
+    }
+};
